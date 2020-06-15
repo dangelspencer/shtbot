4640d97cfba670227551751ada867b2fd997392c
Spencer Dangel <spencerdangel@sevenhillstechnology.com>
2020-06-15 11:39:18 -0400

    ignore @ mentions in /scrabble and /mock commands

--- helpers/text-converter.js
+++ helpers/text-converter.js
@@ -1,36 +1,49 @@
 class TextConverterHelper {
     static async textToScrabbleTiles(text) {
-        let result = '';
+        let convertedWords = [];
         let numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
 
-        for (let i = 0; i < text.length; i++) {
-            let char = text.charAt(i).toLowerCase();
+        const words = text.split(' ');
+        for (const word of words) {
+            let result = '';
 
-
-            // handle numbers
-            if (!isNaN(parseInt(char, 10))) {
-                result += `:${numbers[parseInt(char, 10)]}:`;
+            if (word.startsWith('<@') || word.startsWith('<#')) {
+                convertedWords.push(word);
                 continue;
             }
 
-            // handle spaces
-            if (char === ' ') {
-                result += '   ';
-                continue;
+            for (let i = 0; i < word.length; i++) {
+                let char = text.charAt(i).toLowerCase();
+    
+    
+                // handle numbers
+                if (!isNaN(parseInt(char, 10))) {
+                    result += `:${numbers[parseInt(char, 10)]}:`;
+                    continue;
+                }
+    
+                // handle spaces
+                if (char === ' ') {
+                    result += '   ';
+                    continue;
+                }
+    
+                // filter out punctuation
+                if (!char.match(/[a-zA-Z]/i)) {
+                    continue;
+                }
+    
+                // handle letters
+                char = ['a', 'b', 'm', 'o', 'v', 'x'].includes(char) ? char+char : char;
+                result += (`:${char}:`);
             }
 
-            // filter out punctuation
-            if (!char.match(/[a-z]/i)) {
-                continue;
-            }
-
-            // handle letters
-            char = ['a', 'b', 'm', 'o', 'v', 'x'].includes(char) ? char+char : char;
-            result += (`:${char}:`);
+            convertedWords.push(result);
         }
 
-        return result.trim();
+        return convertedWords.join('   ');
     }
+
     static async textToMockingText(text) {
         let result = '';
         let nonLetterCount = 0; //used to ignore non letters when alternating upper and lower case
@@ -44,6 +57,41 @@ class TextConverterHelper {
         }
 
         return result;
+    }
+    
+    static async textToMockingText(text) {
+        let convertedWords = [];
+        let uppercase = false;
+
+        const words = text.split(' ');
+
+        for (const word of words) {
+            let result = '';
+
+            if (word.startsWith('<@') || word.startsWith('<#')) {
+                convertedWords.push(word);
+                continue;
+            }
+
+            for (let i = 0; i < word.length; i++) {
+                // filter out punctuation
+                if (!word.charAt(i).match(/[a-zA-Z0-9]/i)) {
+                    continue;
+                }
+
+                if (uppercase) {
+                    result += word.charAt(i).toUpperCase();
+                    uppercase = !uppercase;
+                } else {
+                    result += word.charAt(i).toLowerCase();
+                    uppercase = !uppercase;
+                }
+            }
+
+            convertedWords.push(result);
+        }
+
+        return convertedWords.join(' ');
     }
 }
 
