54fbe73854bba8c53fcc542816e68c075199e4d4
Spencer Dangel <spencerdangel@sevenhillstechnology.com>
2020-10-15 21:00:31 -0400

    use server port and host config values

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LoggerService } from './logger.service';
+import { config } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: new LoggerService()
   });
 
-  await app.listen(3000);
+  await app.listen(config.server.port, config.server.host);
 }
 
 bootstrap();
